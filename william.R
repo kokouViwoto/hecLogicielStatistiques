9da210885c09aa3712ca295e256fc0346b248453
william <nzoukou@gmail.com>
2016-10-10 11:12:43 -0400

    finalizing work

--- william.R
+++ william.R
@@ -6,6 +6,7 @@
 
 #import packages
 library(plyr)
+library(ggplot2)
 #library(countrycode)
 
 # Read data - We assume the data are located in a folder named data placed in the same location as the code
@@ -43,9 +44,19 @@ errors = list( "(Bolivia"="Bolivia", "(Russia"="Russia",
                "Dominica"="Dominican Republic", "Hunary"="Hungary","Virginia."="Virginia", "Vienna"="Austria",
                "the Mediterranean"="Mediterranean Sea", "Thiland"="Thailand", "Moscow"="Russia",
                "Massachutes"="Massachusetts","Louisana"="Louisiana","Jamacia"="Jamaica",
-               "Inodnesia"="Indonesia", "Amsterdam"="Netherlands"
+               "Inodnesia"="Indonesia", "Amsterdam"="Netherlands", "Zaïre"="Zaire",
+               "Yugosalvia"="Yugoslavia", "Washingon"="Washington", "U.S. Virgin Islands"="US Virgin Islands",
+               "UK"="United Kingdom", "UAE"="United Arab Emirates", "Tennesee"="Tennessee",
+               "South Africa (Namibia)"="Namibia", "Russian"="Russia", "Rico"="Porto Rico",
+               "Qld. Australia"="Australia", "Queensland  Australia"="Australia", 
+               "Phillipines"="Philippines", "Philipines"="Philippines", "New York (Idlewild)"="New York",
+               "Morroco"="Morrocco", "Minnisota"="Minnesota", "Yukon Territory"="Yukon",
+               "Surinam"="Suriname", "South Dekota"="South Dakota", "Republiof Djibouti"="Djibouti",
+               "Republiof Djibouti"="Georgia_", "Georgia"="Georgia_", "Okinawa"="Japan", "Oklohoma"="Oklahoma"
                )
-sea_words = c('Sea', 'Ocean', 'Channel', 'Mediterranean', 'miles', 'Gulf', 'Strait', 'off')
+sea_words = c('Sea', 'Ocean', 'Channel', 'Mediterranean', 'miles', 'Gulf', 'Strait', 'off', "NE of")
+
+colors = c("red", "yellow", "green", "violet", "orange", "blue", "pink", "cyan") 
 
 fix_country_name <- function(input){  # return corrected country if possible
   if(input %in% names(errors))
@@ -118,36 +129,68 @@ crashes = as.data.frame( t(
  )
 )
 
-# crash frequencies
-crash_freq = count(crashes, 'CrashCountry')
-crash_freq = crash_freq[order(crash_freq$freq, decreasing = TRUE),]
-names(crash_freq)[names(crash_freq) == 'freq'] <- 'Plane_Crashes_by_Country'
-
-cat_freq = count(crashes, 'Category')
-cat_freq = cat_freq[order(cat_freq$freq, decreasing = TRUE),]
-names(cat_freq)[names(cat_freq) == 'freq'] <- 'Plane_Crashes_by_Category'
-
-sur_freq = count(crashes, c('Surface','Category'))
-sur_freq = sur_freq[order(sur_freq$freq, decreasing = TRUE),]
-names(sur_freq)[names(sur_freq) == 'freq'] <- 'Plane_Crashes_Per_Surface'
-
-mil_loc_freq = count( crashes[crashes$Category=='Military', ] , 'CrashCountry' )
-mil_loc_freq = mil_loc_freq[order(mil_loc_freq$freq, decreasing = TRUE),]
-names(mil_loc_freq)[names(mil_loc_freq) == 'freq'] <- 'Military_Plane_Crashes_per_Country'
-
-
 # Questions:
 # 1. Quelles sont les Top 10 des destinations dangereuses (souligner la plus dangereuse)?  Bar chart (destination / nombre d’accidents) (william)
+
 # accidents / pays du crash
+crash_freq = count(crashes, 'CrashCountry')
+crash_freq = as.data.frame(crash_freq[order(crash_freq$freq, decreasing = TRUE),])
+names(crash_freq)[names(crash_freq) == 'freq'] <- 'Plane_Crashes_by_Country'
+top10_crash_freq = head(crash_freq, n = 10)
+#TODO: Chart + Explanation
+
 # accidents / destinations
-# accidents / countries by flight categories
+
+# accidents / countries / flight categories
+cr_cat_freq = count(crashes, c('CrashCountry','Category'))
+cr_cat_freq = as.data.frame(cr_cat_freq[order(cr_cat_freq$freq, decreasing = TRUE),])
+names(cr_cat_freq)[names(cr_cat_freq) == 'freq'] <- 'Number_of_Planes_Crashes'
+top10_cr_cat_freq = head(cr_cat_freq, n = 10)
+#TODO: Chart + Explanation
 
 #accident 
 # where does military planes crash the most - USA because they have more planes?
 # commercial plane
 
+
 # 2. Quel type d’avion a connu le plus de crash? Creer des categories nous memes. (william)
 # nombre de crash ou l'avion est de type commercial VERSUS nombre de crash ou l'avion est de type militaire
+cat_freq = count(crashes, 'Category')
+cat_freq = as.data.frame(cat_freq[order(cat_freq$freq, decreasing = TRUE),])
+names(cat_freq)[names(cat_freq) == 'freq'] <- 'Plane_Crashes_by_Category'
+#TODO: Chart + Explanation
+
+# Do planes have the most accidents at Sea or in Land
+sur_freq = count(crashes, c('Surface','Category'))
+sur_freq = as.data.frame(sur_freq[order(sur_freq$freq, decreasing = TRUE),])
+names(sur_freq)[names(sur_freq) == 'freq'] <- 'Plane_Crashes_Per_Surface'
+#TODO: Chart + Explanation
+
+# Which COuntry has the most military plane crashes
+mil_loc_freq = count( crashes[crashes$Category=='Military', ] , 'CrashCountry' )
+mil_loc_freq = as.data.frame(mil_loc_freq[order(mil_loc_freq$freq, decreasing = TRUE),])
+names(mil_loc_freq)[names(mil_loc_freq) == 'freq'] <- 'Military_Plane_Crashes_per_Country'
+#TODO: Chart + Explanation
 
 # 3. Quel est le pourcentage de chance de survie lors d’un crash. (Celui ci dépendra du type d’avions. Combien de passagers, destinations )? (william)
 # survivants = aboard - fatalities
+#TODO Aggregate on means + Chart + Explanation
+
+
+
+
+#counts <- table(crashes$CrashCountry)
+#barplot(counts, main="Car Distribution", 
+#        xlab="Number of Gears")
+
+
+#q = table(head(arrange(crash_freq,desc(Plane_Crashes_by_Country)), n = 10))
+#barplot(q, main="Car Distribution", 
+#        xlab="Number of Gears")
+
+#p = head(crash_freq, n = 10)
+
+#ggplot(crashes, aes(x = factor(CrashCountry))) + geom_bar(stat = "count")
+
+#p2<-ggplot(mtc,aes(x=factor(gear),y=wt,fill=factor(vs)), color=factor(vs)) +  
+#  stat_summary(fun.y=mean,position="stack",geom="bar")
